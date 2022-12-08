a848ba8b07dce34ddf932338ac8ecdae746ceea6
PC <faceless-stranger@mail.ru>
2022-12-08 18:46:55 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,15 +2,31 @@ package ru.job4j.oop;
 
 public class Cat {
 
-    public String sound() {
-    String voice = "may-may";
-    return voice;
+    private String food; // Создаём поля обьекта
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " " + this.food); // Метод который будет выводить содержимое поле обьекта
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) { // Записываем данные
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Гав");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Черныш");
+        black.show();
     }
 }
