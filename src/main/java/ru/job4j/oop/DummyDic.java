1498ca30987d3711bf7dd79996163dd12465efff
faceless-stranger <116435989+faceless-stranger@users.noreply.github.com>
2022-12-12 13:22:44 +0300

    Update DummyDic.java

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,8 +2,7 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus() {
-        String eng = "Неизвестное слово ";
-        return eng;
+        return "Неизвестное слово " + eng;
     }
 
     public static void main(String[] args) {
