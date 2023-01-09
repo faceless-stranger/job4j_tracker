ba46d7eb5e82df18e599f2614bb869ca3febf769
PC <faceless-stranger@mail.ru>
2023-01-09 17:40:22 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -30,5 +35,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCreated() {
+        return created.format(formatter);
     }
 }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item date = new Item();
+        System.out.println(date.getCreated());
+    }
+}
