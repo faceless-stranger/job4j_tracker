d2e15817c3b4954ec166d4bb6f9399a4dde5dceb
PC <faceless-stranger@mail.ru>
2023-01-09 18:40:09 +0300

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+    private int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+
+    Broken() {
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+}
