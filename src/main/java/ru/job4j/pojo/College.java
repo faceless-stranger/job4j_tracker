82d4acf2b6325d816fa052c0c76db283bccde64b
PC <faceless-stranger@mail.ru>
2023-01-09 21:09:38 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Vlad");
+        student.setGroup("build");
+        student.setDate("11.11.11");
+        System.out.println("Имя " + student.getName() + System.lineSeparator() + "Группа  " + student.getGroup() + System.lineSeparator() + "Дата поступления  " + student.getDate());
+    }
+}
