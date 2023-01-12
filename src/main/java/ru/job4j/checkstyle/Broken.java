d7d76dd736bf2fe156e7dd14370369ea5f29c014
PC <faceless-stranger@mail.ru>
2023-01-12 21:16:56 +0300

    9. Правила Checkstyle [#504981 #379244]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -12,11 +12,11 @@ public class Broken {
     public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 
-    void echo() {
+    public void echo() {
 
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
