09e2892891fadcc2d57fe6157bd8cca9a39be4e7
PC <faceless-stranger@mail.ru>
2023-01-18 18:27:02 +0300

    правки

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,7 +1,6 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
-    private int passengers;
 
     @Override
     public void drive() {
@@ -10,7 +9,7 @@ public class Bus implements Transport {
 
     @Override
     public void passengers(int count) {
-        this.passengers = count;
+
     }
 
     @Override
