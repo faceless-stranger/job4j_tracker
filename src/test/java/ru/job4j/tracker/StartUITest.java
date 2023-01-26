53437657e18861d92dc61f32f31ae6e9ded1aa7d
PC <faceless-stranger@mail.ru>
2023-01-26 15:40:59 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -145,8 +145,9 @@ class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString()).isEqualTo(
-                "Menu." + System.lineSeparator()
+                "Menu:" + System.lineSeparator()
                         + "0. Exit Program" + System.lineSeparator()
+                        + "=== Exit Program ===" + System.lineSeparator()
         );
     }
 }
