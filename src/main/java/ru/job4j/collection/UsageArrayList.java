539eceef510a06372f49f723dba1c26d06041633
PC <faceless-stranger@mail.ru>
2023-02-17 16:55:54 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> collection = new ArrayList<>();
+        collection.add("Petr");
+        collection.add("Ivan");
+        collection.add("Stepan");
+        for (Object value: collection) {
+            System.out.println(value);
+        }
+    }
+}
