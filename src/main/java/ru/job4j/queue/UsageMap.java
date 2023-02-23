e83234f3ccf5abf4156f287d1c6fe58226b5055e
PC <faceless-stranger@mail.ru>
2023-02-23 15:06:15 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/queue/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.queue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> mail = new HashMap<>();
+        mail.put("Gena@yandex.ru", "Гена Викторович Полкотов ");
+        mail.put("Jenya@yandex.ru", "Женя Александрович Хрустов");
+        mail.put("Misha@yandex.ru", "Михаил Михайлович Михайлов");
+        mail.put("Masha@yandex.ru", "Мария Семеновна Острова");
+        for (Map.Entry<String, String> item : mail.entrySet()) {
+            System.out.println("Почта " + item.getKey() +
+                    " ФИО пользователя " + item.getValue());
+        }
+    }
+}
