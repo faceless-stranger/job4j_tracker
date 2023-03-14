14c1246245738106a13b230b4ad776de36762f2c
PC <faceless-stranger@mail.ru>
2023-03-14 20:26:16 +0300

    2.1. Лямбда блок [#249208]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -1,8 +1,6 @@
 package ru.job4j.lambda;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class AttachmentSort {
     public static void main(String[] args) {
