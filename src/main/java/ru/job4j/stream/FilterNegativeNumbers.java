cdcee92c9e06956c5c22f542dff08884637bf896
PC <faceless-stranger@mail.ru>
2023-04-04 20:01:55 +0300

    7. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -8,9 +8,7 @@ public class FilterNegativeNumbers {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, -4, 33, -11, 0, 6);
-        List<Integer> positive = numbers.stream().filter(
-        element -> element >= 0
-        ).collect(Collectors.toList());
+        List<Integer> positive = numbers.stream().filter(element -> element >= 0).collect(Collectors.toList());
         positive.forEach(System.out::println);
     }
 }
